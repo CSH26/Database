86b1e917e3b6878c014166e0dbb754a75a94a315
SungHyun26 <sghf0202@naver.com>
2016-04-29 16:04:46 +0900

    변수추가

--- app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
+++ app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
@@ -14,7 +14,7 @@ import com.ebookfrenzy.database.provider.MyContentProvider;
  */
 public class MyDBHandler extends SQLiteOpenHelper{
 
-
+    int a;
     //TEST
     private ContentResolver myCR;
 
